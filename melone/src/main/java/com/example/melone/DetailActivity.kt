9c66f9c93aa0849590b9c5501909a8378e915c67
SEYOUNG PARK <seyeongb18@gmail.com>
2024-10-08 15:52:15 +0900

    [melone - 해설 강의 참고] 1. 기존에 무한루프 상태에서 첫번째 곡에서는 이전 곡으로 못넘어가게, 마지막 곡에서는 다음 곡으로 못넘어가게 막음
    2. position 을 setter 로 설정
    - setter 는 onCreate 외부에서만 사용 가능
    - setter 는 가독성은 높여줌
    - 조건문에서 if, else if 만 작성하면 안됨. else 도 작성해야 함
    
    3. 음악 재생(mp3, mp4)은 MediaPlayer 보다는 ExoPlayer 를 사용하는 것을 권장한다.

--- melone/src/main/java/com/example/melone/DetailActivity.kt
+++ melone/src/main/java/com/example/melone/DetailActivity.kt
@@ -1,6 +1,5 @@
 package com.example.melone
 
-import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
@@ -11,39 +10,60 @@ import com.example.melone.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityDetailBinding
-    lateinit var mediaPlayer : MediaPlayer
+    lateinit var mediaPlayer: MediaPlayer
+    lateinit var melonItems: ArrayList<MelonItem>
 
     // setter 사용 -> 변수 상태를 따로 관리하여 유지보수를 용이하게 한다. 변수와 뷰를 바인딩시킨다.
     // 액티비티의 생명주기에 간섭받지 않는다. 상태가 바뀌면 바로 뷰에 반영된다.
     var isPlaying = false
         set(value) {
-            if(value) {
-                binding.ivPlay.setImageDrawable(this.resources.getDrawable(R.drawable.ic_stop, this.theme))
+            if (value) {
+                binding.ivPlay.setImageDrawable(
+                    this.resources.getDrawable(
+                        R.drawable.ic_stop,
+                        this.theme
+                    )
+                )
             } else {
-                binding.ivPlay.setImageDrawable(this.resources.getDrawable(R.drawable.ic_play, this.theme))
+                binding.ivPlay.setImageDrawable(
+                    this.resources.getDrawable(
+                        R.drawable.ic_play,
+                        this.theme
+                    )
+                )
             }
             field = value
         }
 
+    private var currentPosition = 0
+        set(value) {
+            if (value < 0) field = 0
+            else if(value == melonItems[0].tracks.size) field = melonItems[0].tracks.size-1
+            else field = value // 이것 빼먹으면 안됨
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d("detailActivity","onCreate")
+        Log.d("detailActivity", "onCreate")
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // adapter에서 분해되서 온 재료를 ArrayList<MelonItem> 형태로 만들겠다!
-        val melonItems = intent.getSerializableExtra("melonItems") as ArrayList<MelonItem>
-        var currentPosition = intent.getIntExtra("current_position", -1)
+        melonItems = intent.getSerializableExtra("melonItems") as ArrayList<MelonItem>
+        currentPosition = intent.getIntExtra("current_position", -1)
 
         binding.tvSongTitle.text = melonItems[0].tracks[currentPosition].name
         Glide.with(this@DetailActivity).load(melonItems[0].image).into(binding.ivSongThumbnail)
 
         // 재생 버튼을 누르지 않은 상태에서 다음 버튼이나 이전 버튼을 누르게 되면
         // 초기화되지 않은(lateinit) mediaPlayer 을 호출하게 되는 문제를 setOnClickListener 밖으로 빼면서 해결
-        mediaPlayer = MediaPlayer.create(this@DetailActivity, Uri.parse(melonItems[0].tracks[currentPosition].audio))
+        mediaPlayer = MediaPlayer.create(
+            this@DetailActivity,
+            Uri.parse(melonItems[0].tracks[currentPosition].audio)
+        )
 
         binding.ivPlay.setOnClickListener {
-            if(isPlaying) {
+            if (isPlaying) {
                 isPlaying = false
                 mediaPlayer.pause()
             } else {
@@ -55,16 +75,14 @@ class DetailActivity : AppCompatActivity() {
         binding.ivPlayBack.setOnClickListener {
             isPlaying = false
             mediaPlayer.release()
-            if(currentPosition == 0) currentPosition = melonItems[0].tracks.size-1
-            else currentPosition -= 1
+            currentPosition -= 1
             binding.tvSongTitle.text = melonItems[0].tracks[currentPosition].name
         }
 
         binding.ivPlayNext.setOnClickListener {
             isPlaying = false
             mediaPlayer.release()
-            if(currentPosition == melonItems[0].tracks.size-1) currentPosition = 0
-            else currentPosition += 1
+            currentPosition += 1
             binding.tvSongTitle.text = melonItems[0].tracks[currentPosition].name
         }
     }
@@ -76,17 +94,17 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        Log.d("detailActivity","onPause")
+        Log.d("detailActivity", "onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("detailActivity","onStop")
+        Log.d("detailActivity", "onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("detailActivity","onDestroy")
+        Log.d("detailActivity", "onDestroy")
         mediaPlayer.release()
     }
 }
