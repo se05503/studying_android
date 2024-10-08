bbf5ff27b4135cd418a020073105be9cfef05736
SEYOUNG PARK <seyeongb18@gmail.com>
2024-10-08 16:58:34 +0900

    [instagram] init: 초기 세팅

---
+++ instagram/src/main/java/com/example/instagram/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.instagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // SecurityException resolve
+        val dexOutputDir: File = codeCacheDir
+        dexOutputDir.setReadOnly()
+    }
+}
